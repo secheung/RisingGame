f8d450e845348813ea3bf01cf4dff44c783ec264
Samuel Cheung <secheung@uwaterloo.ca>
2013-03-01 02:30:06 -0500

    added shader program start

---
+++ Open2D/src/engine/open2d/renderer/ShaderHandlers.java
@@ -0,0 +1,86 @@
+package engine.open2d.renderer;
+
+import java.util.Hashtable;
+
+import android.opengl.GLES20;
+import android.util.Log;
+
+public class ShaderHandlers {
+	private String vShader;
+	private String fShader;
+	private String attributes[];
+	
+	private int compiledShaderProgram;
+	
+	public ShaderHandlers(String vShader,String fShader, String...attributes){
+		this.vShader = vShader;
+		this.fShader = fShader;
+		
+		this.attributes = attributes;
+	}
+	
+	public int getCompiledShaderProgram(){
+		return compiledShaderProgram;
+	}
+	
+	public void buildShaderProgram(){
+		int vertexShaderHandle = compileShader(GLES20.GL_VERTEX_SHADER,vShader);
+	    int fragmentShaderHandle = compileShader(GLES20.GL_FRAGMENT_SHADER, fShader);
+		
+	    compiledShaderProgram = createAndLinkProgram(vertexShaderHandle,fragmentShaderHandle, attributes);
+	}
+	
+	private int compileShader(int shaderType, String shaderProgram){
+		int shaderHandle = GLES20.glCreateShader(shaderType);
+		
+		if (shaderHandle == 0) 
+			throw new RuntimeException("Error creating shader.");
+		
+    	GLES20.glShaderSource(shaderHandle, shaderProgram);
+    	GLES20.glCompileShader(shaderHandle);
+    	
+    	// Get the compilation status.
+    	final int[] compileStatus = new int[1];
+    	GLES20.glGetShaderiv(shaderHandle, GLES20.GL_COMPILE_STATUS, compileStatus, 0);
+    	
+        // If the compilation failed, delete the shader.
+        if (compileStatus[0] == 0){
+        	Log.e("LessonOpenGLESRenderer", "Error compiling shader: " + GLES20.glGetShaderInfoLog(shaderHandle));
+            GLES20.glDeleteShader(shaderHandle);
+            shaderHandle = 0;
+        }
+	    
+	    return shaderHandle;
+	}
+	
+	public int createAndLinkProgram(final int vertexShaderProgram, final int fragmentShaderProgram, final String[] attribute){
+	    // Create a program object and store the handle to it.
+	    int programHandle = GLES20.glCreateProgram();
+	    
+	    if(programHandle == 0)
+	        throw new RuntimeException("Error creating program.");
+	    
+    	GLES20.glAttachShader(programHandle, vertexShaderProgram);
+    	GLES20.glAttachShader(programHandle, fragmentShaderProgram);
+    	
+    	if(attribute != null) {
+    		for(int i = 0; i < attribute.length; i++) {
+		    	GLES20.glBindAttribLocation(programHandle, i, attribute[i]);
+    		}
+    	}
+    	
+    	GLES20.glLinkProgram(programHandle);
+    	
+        final int[] linkStatus = new int[1];
+        GLES20.glGetProgramiv(programHandle, GLES20.GL_LINK_STATUS, linkStatus, 0);
+     
+        if (linkStatus[0] == 0) {
+        	Log.e("LessonOpenGLESRenderer", "Error compiling shader: " + GLES20.glGetShaderInfoLog(programHandle));
+            GLES20.glDeleteProgram(programHandle);
+            programHandle = 0;
+        }
+	    
+	    return programHandle;
+	}
+	
+}
--- Open2D/src/engine/open2d/renderer/WorldRenderer.java
+++ Open2D/src/engine/open2d/renderer/WorldRenderer.java
@@ -1,38 +1,58 @@
 package engine.open2d.renderer;
 
+import java.util.Hashtable;
+
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 import android.content.Context;
+import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
+import android.util.Log;
 import android.view.MotionEvent;
 
 public class WorldRenderer implements GLSurfaceView.Renderer{
+	private final static int BYTESPERFLOATS = 4;
+	
 	Context activityContext;
+	
+	private float[] modelMatrix = new float[16];
+	private float[] viewMatrix = new float[16];
+	private float[] projectionMatrix = new float[16];
+	
+	private float[] MVMatrix = new float[16];
+	private float[] MVPMatrix = new float[16];
+	
+	ShaderHandlers shaders;
 	
 	public WorldRenderer(final Context activityContext) {
 		this.activityContext = activityContext;
 		
 	}
 	
+	public void initSetup(){
+		GLES20.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+		
+		GLES20.glEnable(GLES20.GL_CULL_FACE);
+		GLES20.glEnable(GLES20.GL_DEPTH_TEST);
+		GLES20.glEnable(GLES20.GL_BLEND);
+		GLES20.glBlendFunc(GLES20.GL_SRC_ALPHA, GLES20.GL_ONE_MINUS_SRC_ALPHA);
+		
+	    Matrix.setLookAtM(viewMatrix, 0, 0.0f, 0.0f, -0.5f, 0.0f, 0.0f, -5.0f, 0.0f, 1.0f, 0.0f);
+	    
+	    shaders.buildShaderProgram();
+	}
+	
 	public void passTouchEvents(MotionEvent e){}
 
 	@Override
-	public void onDrawFrame(GL10 gl) {
-		// TODO Auto-generated method stub
-		
-	}
+	public void onDrawFrame(GL10 gl) {}
 
 	@Override
-	public void onSurfaceChanged(GL10 gl, int width, int height) {
-		// TODO Auto-generated method stub
-		
-	}
+	public void onSurfaceChanged(GL10 gl, int width, int height) {}
 
 	@Override
-	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-		// TODO Auto-generated method stub
-		
-	}
+	public void onSurfaceCreated(GL10 gl, EGLConfig config) {}
 	
 }

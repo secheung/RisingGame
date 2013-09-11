8ec1d11cde3f069014f13cb9618950542c75717d
Samuel Cheung <samuel.e.cheung@gmail.com>
2013-09-11 00:21:57 -0400

    improve enemy AI
    move back when other opponent is attacking
    doesn't track(keeps direction) player when attacking

--- RisingGame/src/object/Enemy.java
+++ RisingGame/src/object/Enemy.java
@@ -23,6 +23,7 @@ public class Enemy extends GameObject {
 		ENDSTRIKE("end strike"),
 		RUN("run"),
 		WALK("walk"),
+		WALK_BACK("backup"),
 		DODGE("dodge"),
 		DISABLE("freeze"),
 		DEAD("dead");
@@ -43,10 +44,12 @@ public class Enemy extends GameObject {
 	private static float FAR_DIST_TO_PLAYER = 2.5f;
 	private static float CLOSE_DIST_TO_PLAYER = 1.5f;
 	private static float COLLISION_BUFFER = 1.0f;
+	private static float WALK_BACK_ANIM_PLAYS = 3;
 	
 	Player playerRef;
 	EnemyState enemyState;
 	public int struck;
+	public int walkBackCounter;
 	
 	public Enemy(LinkedHashMap<String,GameObject> gameObjects, Player player, int index, float x, float y, float width, float height){
 		super(gameObjects,x,y,width,height);
@@ -57,11 +60,13 @@ public class Enemy extends GameObject {
 		animations.put(EnemyState.STAND, new Plane(R.drawable.enemy_stance, name+"_"+EnemyState.STAND.getName(), width, height, x, y, z, 4, 7));
 		animations.put(EnemyState.RUN, new Plane(R.drawable.enemy_run, name+"_"+EnemyState.RUN.getName(), width, height, x, y, z, 11, 3));
 		animations.put(EnemyState.WALK, new Plane(R.drawable.enemy_run, name+"_"+EnemyState.WALK.getName(), width, height, x, y, z, 11, 3));
+		animations.put(EnemyState.WALK_BACK, new Plane(R.drawable.enemy_run, name+"_"+EnemyState.WALK_BACK.getName(), width, height, x, y, z, 11, 3));
 		animations.put(EnemyState.DEAD, new Plane(R.drawable.enemy_stance, name+"_"+EnemyState.DEAD.getName(), width, height, x, y, z, 4, 7));
 		animations.put(EnemyState.STRIKE1, new Plane(R.drawable.enemy_strike1, name+"_"+EnemyState.STRIKE1.getName(), width, height, x, y, z, 4, 6));
 		animations.put(EnemyState.STRUCK1, new Plane(R.drawable.enemy_struck1, name+"_"+EnemyState.STRUCK1.getName(), width, height, x, y, z, 2, 7));
 		
 		struck = 3;
+		walkBackCounter = 0;
 		
 		display = animations.get(EnemyState.STAND);
 		enemyState = EnemyState.STAND;
@@ -72,10 +77,12 @@ public class Enemy extends GameObject {
 	public void updateState() {
 		float checkX = getMidX();
 		
-		if(playerRef.getMidX() > checkX){
-			direction = Direction.RIGHT;
-		} else if(playerRef.getMidX() < checkX){
-			direction = Direction.LEFT;
+		if(!isEnemyStriking()){
+			if(playerRef.getMidX() > checkX){
+				direction = Direction.RIGHT;
+			} else if(playerRef.getMidX() < checkX){
+				direction = Direction.LEFT;
+			}
 		}
 
 		if(enemyState == EnemyState.STAND){
@@ -94,10 +101,12 @@ public class Enemy extends GameObject {
 			executeMovement();
 		}
 		
-		if(selected){
+		otherAIMoveInteration();
+		
+		if(selected && GameTools.boxColDetect(this, playerRef, COLLISION_BUFFER)){
 			if(playerRef.getPlayerState() == PlayerState.FINISH){
 				enemyState = EnemyState.DEAD;
-			} else if(playerRef.getPlayerState() == PlayerState.STRIKE1){
+			}else if(playerRef.getPlayerState() == PlayerState.STRIKE1){
 				enemyState = EnemyState.STRUCK1;
 			} else if(playerRef.getPlayerState() == PlayerState.STRIKE2){
 				enemyState = EnemyState.STRUCK1;
@@ -111,20 +120,23 @@ public class Enemy extends GameObject {
 	public void updateLogic() {
 		if(enemyState == EnemyState.STRUCK1){
 			selected = false;
-		}
-		
-		if(enemyState == EnemyState.RUN){
+		}else if(enemyState == EnemyState.RUN){
 			if(direction == Direction.RIGHT)
 				x += RUN_SPEED;
 			else if(direction == Direction.LEFT)
 				x -= RUN_SPEED;
-		}
-		
-		if(enemyState == EnemyState.WALK){
+		}else if(enemyState == EnemyState.WALK){
 			if(direction == Direction.RIGHT)
 				x += WALK_SPEED;
 			else if(direction == Direction.LEFT)
 				x -= WALK_SPEED;
+		}else if(enemyState == EnemyState.WALK_BACK){
+			if(direction == Direction.RIGHT)
+				x -= WALK_SPEED;
+			else if(direction == Direction.LEFT)
+				x += WALK_SPEED;
+			
+			walkBackCounter++;
 		}
 	}
 
@@ -141,18 +153,23 @@ public class Enemy extends GameObject {
 		
 		if(enemyState == EnemyState.DEAD){
 			display.drawDisable();
-		}else if(display.isPlayed() && enemyState == EnemyState.STRUCK1){
-			display.resetAnimation();
-			struck -= 1;
-			enemyState = EnemyState.STAND;
 		}
 	}
 
 	@Override
 	public void updateAfterDisplay() {
 		if(display.isPlayed()){
-			if(enemyState == EnemyState.STRIKE1){
+			if(isEnemyStriking()){
 				display.resetAnimation();
+				enemyState = EnemyState.STAND;
+			} else if(enemyState == EnemyState.WALK_BACK){
+				if(walkBackCounter >= 3){
+					enemyState = EnemyState.STAND;
+					walkBackCounter = 0;
+				}
+			} else if(enemyState == EnemyState.STRUCK1){
+				display.resetAnimation();
+				struck -= 1;
 				enemyState = EnemyState.STAND;
 			}
 		}
@@ -184,6 +201,27 @@ public class Enemy extends GameObject {
 		
 		if(GameTools.boxColDetect(this, playerRef, COLLISION_BUFFER)){
 			enemyState = EnemyState.STAND;
+		}
+	}
+	
+	public void otherAIMoveInteration(){
+		for(GameObject gameObject : gameObjects.values()){
+			if(gameObject instanceof Enemy){
+				Enemy enemy = (Enemy)gameObject;
+				if(enemy != this && GameTools.boxColDetect(this, enemy, COLLISION_BUFFER)){
+					if(enemy.getEnemyState() != EnemyState.WALK && enemy.getEnemyState() != EnemyState.RUN && enemy.getEnemyState() != EnemyState.WALK_BACK){
+						enemyState = EnemyState.WALK_BACK;
+					}
+				}
+			}
+		}
+	}
+	
+	public boolean isEnemyStriking(){
+		if(enemyState == EnemyState.STRIKE1){
+			return true;
+		} else {
+			return false;
 		}
 	}
 	
